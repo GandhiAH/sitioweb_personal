5ff605b687894091db30e4143ce25f63b482efed
GandhiAH <gandhi.agusxd@gmail.com>
2025-11-22 12:35:17 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <title>Gandhi Aguas</title>
+            <link rel="stylesheet" href="estilos.css">
+        </head>
+    <body>
+        <div class="menus">
+            <a href="index.php" class="btn">INICIO</a>
+            <a href="index.php" class="btn">ACERCA DE</a>
+            <a href="index.php" class="btn">COMERCIO</a>
+            <a href="index.php" class="btn">SERVICIOS</a>
+            <a href="index.php" class="btn">BLOG</a>
+            <a href="contacto.php" class="btn">CONTACTO</a>
+        </div>
+
+        <header>
+            <h1>¡BIENVENIDO! SOY GANDHI AGUAS</h1>
+        </header>
+        <div class="centrar">
+            <section class="bio">
+            <img src="gandhifoto.jpeg" width="350px" alt="Gandhi Foto">
+
+                <div class="informacion">
+                    <h2>CONOCE A GANDHI AGUAS</h2>
+                    <p>Hola, Me llamo Gandhi Aguas, soy un estudiante de la Universidad Tecnica Particular de Loja (UTPL). Me considero una persona amable, responsable y siempre dispuesto a aprender cosas nuevas, Me apasiona la tecnología, los videojuegos y todo lo relacionado con el mundo de la programación ya que me permite crear, resolver problemas y desarrollar ideas que pueden ayudar a otros. Disfruto mucho pasar tiempo con mi familia, jugar juegos para divertirme, conversar y compartir momentos con mis amigos. También me gusta salir a caminar al aire libre para despejar la mente, observar la naturaleza y encontrar inspiración, creo que el equilibrio entre estudiar, aprender en mis metas y dedicar tiempo a quienes quiero es fundamental para crecer tanto personal como profesionalmente.</p>
+                    <a href="index.php" class="leer-mas">Leer más</a>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
